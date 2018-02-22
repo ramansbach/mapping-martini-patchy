58cdb78fedafcd054d8d65732a2bc0aba8b51154
Rachael Mansbach <ramansbach@gmail.com>
2018-02-22 15:26:27 -0600

    added some more files, continued work on python script

--- createMartiniModel.py
+++ createMartiniModel.py
@@ -37,6 +37,146 @@ class Bead:
         self.pos = pos
         self.vel = vel
         self.btype = btype
+        
+class Bond:
+    """
+    A class that keeps track of the 2-body interactions for a topology
+    (bonds and constraints)
+    
+    ----------
+    Attributes
+    ----------
+    ainds: [bead,bead]
+        the constituent atoms
+    params: [int,num1,num2]
+        the bond parameters (int is the bond type, num1 is either the bond 
+        length or the table lookup number, num2 is either the bond strength
+        or 1)
+    notes: free form string
+        any comment to go after the end, defaults to ''
+    """    
+    def __init__(self,ainds,params,notes = ''):
+        """
+        Create a bond given two atoms and some parameters
+        """
+        self.ainds = ainds
+        self.params = params
+        self.notes = notes
+    
+    def __str__(self):
+        """
+        Create a string representation as this bond should be written out
+        in an itp file
+        """
+        s = ''
+        for a in self.ainds:
+            s += '\t'
+            s += str(a)
+        for p in self.params:
+            s += '\t'
+            s += str(p)
+        s += '; '+ str(self.notes) + '\n'
+
+        return s
+    
+class Angle(Bond):
+    """
+    A class that keeps track of the 3-body interactions (angles) for a
+    topology file
+    
+    -------
+    Parents
+    -------
+    Bond (same as bond except parameters and ainds are longer lists)
+    
+    """
+    def __init__(self,ainds,params,notes=''):
+        Bond.__init__(ainds,params,notes)
+    
+
+class Dihedral(Angle):
+    """
+    A class that tracks the 4-body interactions (dihedrals) for a topology file
+    
+    -------
+    Parents
+    -------
+    Angle -- identical except it has 
+    """  
+    def __init__(self,ainds,params,notes=''):
+        Bond.__init__(ainds,params,notes)
+        
+class Itp:
+    """
+    A class that holds different parts of the .itp file format ready for 
+    writing
+
+    ----------
+    Attributes
+    ----------
+    moltype:  [string,int]
+        name, exclusions
+    chemName: string
+        short version of chemistry name (ie DFAG)
+    atomlist: list of beads containing structural information about each atom
+    bondlist: list of bonds containing structural information about each bond
+    conlist: list of constraints containing structural information about each 
+        one
+    anglist: list of angles containing structural information about each one
+    dihlist: list of dihedrals containing structural information about each one
+    
+    """
+    def __init__(self,chemName,moltype,atomlist,bondlist,conlist,anglist,
+                 dihlist):
+        """
+        Initialize attributes
+        """
+        self.chemName = chemName
+        self.moltype = moltype
+        self.atomlist = atomlist
+        self.bondlist = bondlist
+        self.conlist = conlist
+        self.anglist = anglist
+        self.dihlist = dihlist
+        
+    def write(self,fname):
+        """
+        write out an itp file
+        
+        ----------
+        Parameters
+        ----------
+        fname: string
+            name of file to be written to
+        """
+        fid = open(fname,'w')
+        fid.write('; MARTINI (martini22) Coarse Grained topology file for \
+                  "Protein"\n')
+        fid.write('; written by createMartiniModel for ' + self.chemName+\
+                  ' chemistry\n')
+        fid.write('\n')
+        fid.write('[ moleculetype ]\n')
+        fid.write('; Name         Exclusions\n')
+        fid.write('{0}\t\t{1}\n'.format(self.moltype[0],self.moltype[1]))
+        fid.write('[ atoms ]\n')
+        for atom in self.atomlist:
+            fid.write(str(atom))
+        fid.write('[ bonds ]\n')
+        for bond in self.bondlist:
+            fid.write(str(bond))
+        
+        fid.write('[ constraints ]\n')
+        for constraint in self.conlist:
+            fid.write(str(constraint))
+            
+        fid.write('[ angles ]\n')
+        for angle in self.anglist:
+            fid.write(str(angle))
+            
+        fid.write('[ dihedrals ]\n')
+        for dih in self.dihlist:
+            fid.write(str(dih))
+        fid.close()
     
 
 class Gro:
@@ -79,16 +219,6 @@ class Gro:
         fid.write('{0} {1} {2}\n'.format(self.box[0],self.box[1],self.box[2]))
         fid.close()
 
-class Itp:
-    """
-    A class that holds the different parts of an itp file for construction
-    and writing
-    
-    ----------
-    Attributes
-    ----------
-    
-    """        
 
 
 def main():
@@ -124,7 +254,7 @@ def main():
     pdbname = args.pdbname
     itpname = args.itpname
     (Itp,Gro) = constructTopology(residues,symmetry)
-    Pdb.write(pdbname)
+    Gro.write(pdbname)
     Itp.write(itpname)
 
 if __name__ == "__main__":
