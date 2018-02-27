3df4f8eacb2b47c78df707cac5e8df22c25d7b4a
Rachael Mansbach <ramansbach@gmail.com>
2018-02-27 15:51:15 -0600

    started writing main model file in earnest added some files for testing and for running the script

--- createMartiniModel.py
+++ createMartiniModel.py
@@ -4,7 +4,11 @@ Created on Thu Feb 22 09:30:04 2018
 
 @author: rachael
 """
-import argparse
+from __future__ import absolute_import, division, print_function
+import argparse,numpy as np
+import pdb
+from martini22_ff import martini22
+from warnings import warn
 
 class Bead:
     """
@@ -28,6 +32,10 @@ class Bead:
         bead/atom velocity
     btype: string
         bead/atom type
+    charge: float
+        charge on the bead, by default 0
+    structure: string
+        secondary structure, by default random coil
     """
     def __init__(self,resNo,resname,name,number,pos,vel,btype):
         self.resNo = resNo
@@ -37,6 +45,23 @@ class Bead:
         self.pos = pos
         self.vel = vel
         self.btype = btype
+        self.charge = 0.0
+        self.structure = 'C'
+        
+    def __str__(self):
+        """
+        Create a string representation as this bead should be written out
+        in an itp file
+        """
+        s = '{:>5}{:>6}{:>6}{:>6}{:>6}{:>6}{:>8.4}; {}\n'.format(self.number,
+                                                                 self.btype,
+                                                                 self.resNo,
+                                                                 self.resname,
+                                                                 self.name,
+                                                                 self.number,
+                                                                 self.charge,
+                                                                 self.structure)
+        return s
         
 class Bond:
     """
@@ -71,13 +96,34 @@ class Bond:
         s = ''
         for a in self.ainds:
             s += '\t'
-            s += str(a)
+            s += str(a.number)
         for p in self.params:
             s += '\t'
             s += str(p)
         s += '; '+ str(self.notes) + '\n'
 
         return s
+        
+    def contains(self,ind):
+        """
+        Check whether any of the atoms in the bond has a particular index
+        
+        ----------
+        Parameters
+        ----------
+        ind: int
+            index of atom to query
+            
+        -------
+        Returns
+        -------
+        c: bool
+            True if atom with index ind found, false else
+        """
+        for a in self.ainds:
+            if a.number == ind:
+                return True
+        return False
     
 class Angle(Bond):
     """
@@ -91,7 +137,7 @@ class Angle(Bond):
     
     """
     def __init__(self,ainds,params,notes=''):
-        Bond.__init__(ainds,params,notes)
+        Bond.__init__(self,ainds,params,notes)
     
 
 class Dihedral(Angle):
@@ -101,10 +147,10 @@ class Dihedral(Angle):
     -------
     Parents
     -------
-    Angle -- identical except it has 
+    Angle -- identical except it has long ainds and params 
     """  
     def __init__(self,ainds,params,notes=''):
-        Bond.__init__(ainds,params,notes)
+        Bond.__init__(self,ainds,params,notes)
         
 class Itp:
     """
@@ -157,25 +203,27 @@ class Itp:
         fid.write('\n')
         fid.write('[ moleculetype ]\n')
         fid.write('; Name         Exclusions\n')
-        fid.write('{0}\t\t{1}\n'.format(self.moltype[0],self.moltype[1]))
+        fid.write('{0}\t\t{1}\n\n'.format(self.moltype[0],self.moltype[1]))
         fid.write('[ atoms ]\n')
         for atom in self.atomlist:
             fid.write(str(atom))
+        fid.write('\n')
         fid.write('[ bonds ]\n')
         for bond in self.bondlist:
             fid.write(str(bond))
-        
+        fid.write('\n')
         fid.write('[ constraints ]\n')
         for constraint in self.conlist:
             fid.write(str(constraint))
-            
+        fid.write('\n')
         fid.write('[ angles ]\n')
         for angle in self.anglist:
             fid.write(str(angle))
-            
+        fid.write('\n')
         fid.write('[ dihedrals ]\n')
         for dih in self.dihlist:
             fid.write(str(dih))
+        fid.write('\n')
         fid.close()
     
 
@@ -219,7 +267,372 @@ class Gro:
         fid.write('{0} {1} {2}\n'.format(self.box[0],self.box[1],self.box[2]))
         fid.close()
 
+class Blist:
+    """
+    A list containing a list of Bonds with some added useful features
+    
+    ----------
+    Attributes
+    ----------
+    
+    """
+    def __init__(self):
+        self.entries = []
+    
+    def __getitem__(self,key):
+        return self.entries[key]
+        
+    def __setitem__(self,key,item):
+        self.entries[key] = item
+    
+    def __len__(self):
+        return len(self.entries)
+        
+    def __str__(self):
+        return str([str(entry) for entry in self.entries])
+        
+    def append(self,entry):
+        """
+        Append a bond to the list
+        
+        ----------
+        Parameters
+        ----------
+        entry: a Bond, Angle, or Dihedral object
+        
+        """
+        self.entries.append(entry)
+    
+    def remove(self,entry):
+        """
+        Remove a bond from the list
+        
+        ----------
+        Parameters
+        ----------
+        entry: a Bond, Angle, or Dihedral object
+        
+        """
+        self.entries.remove(entry)
+    
+    def removeByIndex(self,ID):
+        """
+        Removes all entries in the list containing an atom with number ind
+        
+        ----------
+        Parameters
+        ----------
+        ID: int
+        """
+        
+        newEntries = []
+        for entry in self.entries:
+            if not entry.contains(ID):
+                newEntries.append(entry)
+        self.entries = newEntries        
+        
+        
+class Topology:
+    """
+    contains all requisite information for a single-molecule system
+    
+    ----------
+    Attributes
+    ----------
+    title: string
+            Information about the file
+    atomno: int
+            number of atoms in the system
+    box: list of floats, length 3
+            the box vectors of the system (assume cubic box)
+    moltype:  [string,int]
+        name, exclusions
+    chemName: string
+        short version of chemistry name (ie DFAG)
+    atomlist: list of beads containing structural information about each atom
+    bondlist: list of bonds containing structural information about each bond
+    conlist: list of constraints containing structural information about each 
+        one
+    anglist: list of angles containing structural information about each one
+    dihlist: list of dihedrals containing structural information about each one
+    """
+    def __init__(self):
+        self.title = ''
+        self.atomlist = []
+        self.atomno = 0
+        self.box = [0.,0.,0.]
+        self.moltype = ['Protein',1]
+        self.chemName = 'DXXX'
+        self.bondlist = Blist()
+        self.conlist = Blist()
+        self.anglist = Blist()
+        self.dihlist = Blist()
+    
 
+class DXXXTopology(Topology):
+    """
+    Base topology, which inherits from DAAA as the simplest possible homodimer
+    Can replace residues in the side chains
+    
+    ----------
+    Attributes
+    ----------
+     title: string
+            Information about the file
+    atomno: int
+            number of atoms in the system
+    box: list of floats, length 3
+            the box vectors of the system (assume cubic box)
+    moltype:  [string,int]
+        name, exclusions
+    chemName: string
+        short version of chemistry name (ie DFAG)
+    atomlist: list of beads containing structural information about each atom
+    bondlist: list of bonds containing structural information about each bond
+    conlist: list of constraints containing structural information about each 
+        one
+    anglist: list of angles containing structural information about each one
+    dihlist: list of dihedrals containing structural information about each one
+    Itp: an Itp object
+        mostly for writing
+    Gro: a Gro object
+        mostly for writing
+    """
+    def __bangle__(self,spline,nbeads):
+        """
+        Helper function that creates the parameters for a bond, constraint,
+        or angle
+        
+        ----------
+        Parameters
+        ----------
+        spline: a list of strings
+        nbeads: the number of beads to go into the creation (2 for bond, 
+        constraint, 3 for angle)
+        
+        -------
+        Returns
+        -------
+        beads: a list of Beads
+            the participating beads in the bonded interaction
+        params: a list of the parameters
+        notes: any comments
+        """
+        beads = []
+        for bi in range(0,nbeads):
+            beads.append(self.atomlist[int(spline[bi])-1])
+        params = []
+        if ';' in spline:
+            cind = spline.index(';')
+        else:
+            cind = len(spline)
+        for ci in range(nbeads,cind):
+            params.append(spline[ci])
+        notes = ''
+        for nind in range(cind+1,len(spline)):        
+            notes+= spline[nind]+' '
+      
+        return (beads,params,notes)
+    
+    def clearVars(self):
+        """
+        reset all variables to empty, primarily used for testing purposes
+        """
+        Topology.__init__(self)
+        
+    def __init__(self,itpname,groname):
+        """
+        Initialize topology from a base itp file (preferably DFAG) and grofile
+        
+        ----------
+        Parameters
+        ----------
+        fname: string
+            location of topology file to initialize from
+        """
+        Topology.__init__(self)
+        fid = open(itpname)
+        topology = fid.readlines()
+        fid.close()
+        fid = open(groname)
+        grofile = fid.readlines()
+        grodata = grofile[2:(len(grofile)-1)]
+        box = grofile[len(grofile)-1].split()
+        self.box = [float(box[0]),float(box[1]),float(box[2])]
+        fid.close()
+        flag = 'notes'
+        for line in topology:
+            if line.strip() == '[ moleculetype ]':
+                flag = 'moltype'
+                continue
+            if line.strip() == '[ atoms ]':
+                flag = 'atoms'
+                continue
+            if line.strip() == '[ bonds ]':
+                flag = 'bonds'
+                continue
+            if line.strip() == '[ constraints ]':
+                flag = 'cons'
+                continue
+            if line.strip() == '[ angles ]':
+                flag = 'angles'
+                continue
+            if line.strip() == '[ dihedrals ]':
+                flag = 'dihs'
+                continue
+            if flag == 'notes':
+                self.title += line
+            else:
+                if line[0] == ';' or line == '\n':
+                    continue
+                else:
+                    spline = line.split()
+                    if flag == 'moltype':
+                        self.moltype[0] = spline[0]
+                        self.moltype[1] = int(spline[1])
+                    elif flag == 'atoms':
+                        resNo = int(spline[2])
+                        resname = spline[3]
+                        beadname = spline[4]
+                        beadno = int(spline[0])
+                        beadtype = spline[1]
+                        gline = grodata[beadno-1]
+                        spgline = gline.split()
+                        pos = np.array([float(spgline[3]),float(spgline[4]),
+                                        float(spgline[5])])
+                        vel = np.array([0.,0.,0.])
+                        A = Bead(resNo,resname,beadname,beadno,pos,vel,
+                                 beadtype)
+                        self.atomlist.append(A)
+                    elif flag == 'bonds':
+                        (beads,params,notes) = self.__bangle__(spline,2)
+                        B = Bond(beads,params,notes)
+                        self.bondlist.append(B)
+                    elif flag == 'cons':
+                        (beads,params,notes) = self.__bangle__(spline,2)
+                        C = Bond(beads,params,notes)
+                        self.conlist.append(C)
+                    elif flag == 'angles':
+                        (beads,params,notes) = self.__bangle__(spline,3)
+                        A = Angle(beads,params,notes)
+                        self.anglist.append(A)
+                    elif flag == 'dihs':
+                        (beads,params,notes) = self.__bangle__(spline,4)
+                        D = Dihedral(beads,params,notes)
+                        self.dihlist.append(D)
+        self.Itp = Itp(self.chemName,self.moltype,self.atomlist,self.bondlist,
+                       self.conlist,
+                       self.anglist,self.dihlist)
+        title = 'This file was created by createMartiniModel for the DXXX-OPV3-XXXD system with side residues PHE, ALA, and GLY'
+        self.Gro = Gro(title,len(self.atomlist),self.atomlist,self.box)
+    
+    def write(self,fname):
+        """
+        Write out an itp file, a top file and a gro file corresponding to 
+        the system
+        
+        ----------
+        Parameters
+        ----------
+        fname: string
+            the base name to use for both fname.itp and fname.gro
+        """
+        self.Gro.write(fname+'.gro')
+        self.Itp.write(fname+'.itp') 
+        top = open(fname+'.top','w')
+        top.write('#include "martini.itp"\n')
+        top.write('#include "{}"\n'.format(fname+'.itp'))
+        top.write('[ system ]\n\n')
+        top.write('; name\n')
+        top.write('{} system\n\n'.format(self.chemName))
+        top.write('[ molecules ]\n\n')
+        top.write('; name \t number\n\n')
+        top.write('{} \t {}\n'.format(self.moltype[0],self.moltype[1]))
+    
+    def resSwap(self,name,structure,resID):
+        """
+        Swap out one residue for another.  This is the meat of this whole
+        structure.
+        
+        ----------
+        Parameters
+        ----------
+        name: string
+            name of the residue to be swapped in
+        structure: string
+            the 2ndary structure of the residue to be swapped in
+        resID: int
+            the residue number for the residue to be swapped out
+        
+        -----
+        Notes
+        -----
+        (1) first index is start index
+        (2) remove all atoms, bonds, constraints, angles and dihedrals 
+            containing the atoms in the IDs list.  Make sure to renumber
+            the atoms that come after these atoms
+        (3) add all atoms, bonds, constraints, angles, and dihedrals
+            necessary by the definitions found in Martini 2.2 FF.
+            This includes non-explicit but generic BB angles & bonds
+            (bonds should connect backbone bead to res+1 and res-1;
+            angles should connect backbone bead to res+1,res+2,res-1,res-2)
+            Once again make sure to renumber the atoms that come after these 
+            atoms.
+        """
+        
+        ind0 = self.removeRes(resID)
+        
+        self.addRes(name,structure,ind0)
+    
+        
+    def removeRes(self,resID):
+        """
+        Remove the residue comprised of the atoms with the given resID
+        
+        ----------
+        Parameters
+        ----------
+        IDs: list of ints
+            list of indices
+        
+        -------
+        Returns
+        -------
+        ind0: int
+            index of the first atom ID where the residue started
+        -----    
+        Notes
+        -----
+        * find and remove all bonds, constraints, angles, dihedrals containing
+        atoms with the given indices
+        * find and remove all beads with the given indices
+        * renumber all beads with indices larger than the given indices
+        """
+        IDs = []
+        for atom in self.atomlist:
+            if atom.resNo == resID:
+                IDs.append(atom.number)
+                
+        for ID in IDs:
+            self.bondlist.removeByIndex(ID)
+            self.conlist.removeByIndex(ID)
+            self.anglist.removeByIndex(ID)
+            self.dihlist.removeByIndex(ID)
+        
+        reind = 0
+        
+        natomlist = []
+        for atom in self.atomlist:
+            if atom.number not in IDs:
+                atom.number += reind
+                if atom.resNo > resID:
+                    atom.resNo -= 1
+                natomlist.append(atom)
+            else:
+                reind -= 1
+        self.atomlist = natomlist
+        return min(IDs)
+        
 
 def main():
     """
--- test_create_model.py
+++ test_create_model.py
@@ -8,7 +8,7 @@ Unit tests for createMartiniModel
 """
 import numpy as np,numpy.testing as npt
 from createMartiniModel import *
-
+import pdb
 def test_Bead():
     """
     test bead construction
@@ -21,7 +21,7 @@ def test_Bead():
     assert B.btype == 'SC5'
     npt.assert_array_equal(B.pos,np.array([0.,0.,0.]))
     npt.assert_array_equal(B.vel,np.array([0.,0.,0.]))
-    
+
 def test_write_gro():
     """
     test gro file construction/writing
@@ -29,3 +29,252 @@ def test_write_gro():
     B = Bead(1,'PHE','BB',1,np.array([0.,0.,0.]),np.array([0.,0.,0.]),'SC5')
     G = Gro('testing gro construction',1,[B],[30.3,30.3,30.3])
     G.write('test.gro')
+    
+
+def test_DXXX_bangle():
+    """
+    test helper function for DXXXTopology
+    """
+    btest = ' 1     2      8         3     1 ; GLY(C)-COA(C)\n'
+    ctest = ' 2    3      1   0.20940  1 ; COP 28\n'
+    atest = '   1    2    3      8         5     1 ; COP-COP-VIN\n'
+    Top = DXXXTopology('homoAA.itp','homoA.gro')
+    Top.clearVars()
+    Top.atomlist.append(Bead(1,'TES','BB',1,np.array([0.,0.,0.]),np.array([0.,0.,0.]),'P4'))
+    Top.atomlist.append(Bead(1,'TES','SC1',2,np.array([0.,0.,0.]),np.array([0.,0.,0.]),'SP4'))
+    Top.atomlist.append(Bead(1,'TES','BB',3,np.array([0.,0.,0.]),np.array([0.,0.,0.]),'P4'))
+    (beads,params,notes) = Top.__bangle__(btest.split(),2)
+    (cons,cparams,cnotes) = Top.__bangle__(ctest.split(),2)
+    (angs,aparams,anotes) = Top.__bangle__(atest.split(),3)
+    assert beads[0].number == 1
+    assert beads[0].resNo == 1
+    assert beads[0].resname == 'TES'
+    assert beads[0].name == 'BB'
+    assert beads[0].btype == 'P4'
+    
+    assert beads[1].number == 2
+    assert beads[1].resNo == 1
+    assert beads[1].resname == 'TES'
+    assert beads[1].name == 'SC1'
+    assert beads[1].btype == 'SP4'
+    
+    assert beads[1] == cons[0]
+    assert angs[0] == beads[0]
+    assert angs[1] == cons[0]
+    assert angs[2] == cons[1]
+    
+    assert params[0] == '8'
+    assert params[1] == '3'
+    assert params[2] == '1'
+    
+    assert cparams[0] == '1'
+    assert cparams[1] == '0.20940'
+    assert cparams[2] == '1'
+    
+    assert aparams[0] == '8'
+    assert aparams[1] == '5'
+    assert aparams[2] == '1'
+    
+    assert notes.strip() == 'GLY(C)-COA(C)'
+    assert cnotes.strip() == 'COP 28'
+    assert anotes.strip() == 'COP-COP-VIN'
+  
+    
+def test_Bonds():
+    """
+    create some bonds of different types and test them
+    """
+    Top = DXXXTopology('homoAA.itp','homoA.gro')
+    blist = Top.bondlist
+    clist = Top.conlist
+    alist = Top.anglist
+    dlist = Top.dihlist
+    
+    bcounts = 0
+    for bond in blist:
+        if bond.contains(10):
+            bcounts+=1
+    assert bcounts == 2
+    ccounts = 0
+    for con in clist:
+        if con.contains(9):
+            ccounts +=1
+    assert ccounts==2
+    acounts = 0
+    for ang in alist:
+        if ang.contains(14):
+            acounts +=1
+    assert acounts == 3
+    dcounts = 0
+    for dih in dlist:
+        if dih.contains(9):
+            dcounts += 1
+    assert dcounts == 4*9
+
+def test_Blist():
+    """
+    test the Blist object for containing bonds
+    """
+    B1 = Bead(1,'TES','T',1,np.array([0.,0.,0.]),np.array([0.,0.,0.]),'T')
+    B2 = Bead(1,'TES','T',2,np.array([0.,0.,0.]),np.array([0.,0.,0.]),'T')
+    B3 = Bead(1,'TES','T',3,np.array([0.,0.,0.]),np.array([0.,0.,0.]),'T')
+    Blst = Blist()
+    B12 = Bond([B1,B2],[])
+    B23 = Bond([B2,B3],[])
+    B13 = Bond([B1,B3],[])
+    Blst.append(B12)
+    Blst.append(B23)
+    Blst.append(B13)
+    assert Blst[0] == B12
+    Blst.remove(B12)
+    assert Blst[0] == B23
+    Blst.append(B12)
+    Blst.removeByIndex(3)
+    assert Blst[0] == B12
+    
+def test_DXXXTopology():
+    """
+    Test base initialization of DXXX Topology from file
+    """
+    Top = DXXXTopology('homoAA.itp','homoA.gro')
+    btest = Top.atomlist[4]
+    assert btest.resNo == 3
+    assert btest.resname == 'COP'
+    assert btest.number == 5
+    assert btest.name == 'BB'
+    assert btest.btype == 'SC5'
+    assert btest.charge == 0.0
+    assert btest.structure == 'C'
+    assert Top.conlist[0].ainds[1] == btest
+    assert Top.conlist[0].ainds[0].number == 3
+    assert float(Top.conlist[0].params[1]) ==  0.2094
+    dtest = Top.dihlist[len(Top.dihlist)-1]
+    assert dtest.ainds[0] == Top.atomlist[5]
+    assert dtest.ainds[1] == Top.atomlist[6]
+    assert dtest.ainds[2] == Top.atomlist[8]
+    assert dtest.ainds[3] == Top.atomlist[9]
+    assert dtest.params[0] == '9'
+    assert dtest.params[1] == '90'
+    assert dtest.params[2] == '-0.006706'
+    assert dtest.params[3] == '4'
+    assert len(dtest.params)==4
+    
+def test_DXXXTopology_write():
+    """
+    Make sure there are no serious bugs in the write out portion
+    """
+    Top = DXXXTopology('DFAG.itp','DFAG.gro')
+    Top.write('DFAG_auto')
+    
+def test_removeRes_back():
+    """
+    Test removing residues from a system
+    """
+    Top1 = DXXXTopology('homoAA.itp','homoA.gro')
+    
+    for i in range(2,10):
+        resID = 2
+        Top1.removeRes(resID)
+    assert len(Top1.atomlist) == 1
+    assert len(Top1.bondlist) == 0
+    assert len(Top1.anglist) == 0
+    assert len(Top1.conlist) == 0
+    assert len(Top1.dihlist) == 0
+    assert Top1.atomlist[0].number == 1
+    assert Top1.atomlist[0].resNo == 1
+    assert Top1.atomlist[0].resname == 'ALA'
+    
+def test_removeRes_front():
+    """
+    Test removing residues from a system
+    """
+    Top1 = DXXXTopology('homoAA.itp','homoA.gro')
+    
+    for i in range(1,9):
+        resID = 1
+        Top1.removeRes(resID)
+    assert len(Top1.atomlist) == 1
+    assert len(Top1.bondlist) == 0
+    assert len(Top1.anglist) == 0
+    assert len(Top1.conlist) == 0
+    assert len(Top1.dihlist) == 0
+    assert Top1.atomlist[0].number == 1
+    assert Top1.atomlist[0].resNo == 1
+    assert Top1.atomlist[0].resname == 'ALA'
+
+    
+    
+def test_removeRes_midsingle():
+    Top1 = DXXXTopology('homoAA.itp','homoA.gro')
+    bnum = len(Top1.bondlist)
+    cnum = len(Top1.conlist)
+    anum = len(Top1.anglist)
+    dnum = len(Top1.dihlist)
+    atnum = len(Top1.atomlist)
+    Top1.removeRes(4)
+    assert len(Top1.atomlist) == atnum - 1
+    assert len(Top1.bondlist) == bnum - 2
+    assert len(Top1.conlist) == cnum
+    assert len(Top1.anglist) == anum - 3
+    assert len(Top1.dihlist) == dnum - 4*9
+    assert Top1.atomlist[4].number == 5   
+    assert Top1.atomlist[4].resname == 'COP'
+    assert Top1.atomlist[5].number == 6
+    assert Top1.atomlist[5].resname == 'BEN'
+    assert Top1.bondlist[1].ainds[0].number == 2
+    assert Top1.bondlist[1].ainds[1].number == 3
+    assert Top1.bondlist[2].ainds[0].number == 8
+    assert Top1.bondlist[2].ainds[1].number == 9
+    
+def test_removeRes_midsingle2():
+    Top1 = DXXXTopology('homoAA.itp','homoA.gro')
+    bnum = len(Top1.bondlist)
+    cnum = len(Top1.conlist)
+    anum = len(Top1.anglist)
+    dnum = len(Top1.dihlist)
+    atnum = len(Top1.atomlist)
+    Top1.removeRes(5)
+    assert len(Top1.atomlist) == atnum - 3
+    assert len(Top1.bondlist) == bnum - 2
+    assert len(Top1.conlist) == cnum - 3
+    assert len(Top1.anglist) == anum - 4
+    assert len(Top1.dihlist) == dnum - 5*9
+    assert Top1.atomlist[5].number == 6
+    assert Top1.atomlist[5].resname == 'VIN'
+    assert Top1.atomlist[6].number == 7
+    assert Top1.atomlist[6].resname == 'VIN'
+    
+def test_removeRes_mid1():
+    """
+    Test removing residues from a system
+    """
+    Top1 = DXXXTopology('homoAA.itp','homoA.gro')
+    
+    for i in range(5):
+        resID = 1
+        Top1.removeRes(resID)
+        print("remaining: ")
+        for atom in Top1.atomlist:
+            print(atom)
+        print("-----------\n")
+        #pdb.set_trace()
+    pdb.set_trace()
+    for i in range(2):
+        resID = 3
+        Top1.removeRes(resID)
+        if i == 0:
+            pdb.set_trace()
+    assert len(Top1.atomlist) == 2
+    assert len(Top1.bondlist) == 1
+    assert len(Top1.anglist) == 0
+    assert len(Top1.conlist) == 0
+    assert len(Top1.dihlist) == 0
+    
+    assert Top1.atomlist[0].number == 1
+    assert Top1.atomlist[0].resNo == 1
+    assert Top1.atomlist[0].resname == 'VIN'
+    assert Top1.atomlist[1].number == 2
+    assert Top1.atomlist[1].resNo == 2
+    assert Top1.atomlist[1].resname == 'COA'
+    assert Top1.atomlist[0] == Top1.bondlist[0].ainds[0]
+    assert Top1.atomlist[1] == Top1.bondlist[0].ainds[1]
